74d1f21f46364de89a8f5cba8525eba1223fda50
amritlshidling <amritlshidling@gmail.com>
2026-02-04 21:17:40 +0000

    Logs

--- analytics-service/src/main/java/com/chronos/analyticsservice/consumer/AnalyticsConsumer.java
+++ analytics-service/src/main/java/com/chronos/analyticsservice/consumer/AnalyticsConsumer.java
@@ -1,17 +1,21 @@
 package com.chronos.analyticsservice.consumer;
 
 import com.chronos.common.dto.UserEvent;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.function.Consumer;
 
 @Configuration
+@Slf4j
 public class AnalyticsConsumer {
     @Bean
     public Consumer<UserEvent> userActivityConsumer(){
         return userEvent -> {
-            System.out.println("Processing User: " + userEvent.userId());
+            System.out.println("*********** New Message Received ***********");
+            System.out.println("Payload: " + userEvent);
+            System.out.println("********************************************");
         };
     }
 }
